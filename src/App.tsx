d40afb7f18203a3e195c8802ca706f1f5a220205
lelka00777 <lelka00777@gmail.com>
2022-05-31 17:18:37 +0300

    Ball

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,18 @@ import './App.css';
 import {Circle} from "./Components/Circle/Circle";
 import {NeonSocialIcon} from "./Components/NeonSocialIcon/NeonSocialIcon";
 import {ButtonAnimation} from "./Components/ButtonAnimation/ButtonAnimation";
+import {Ball} from "./Components/Ball/Ball";
 
 function App() {
 
 
     return (
         <div className="App">
-
-            <NeonSocialIcon />
-
-
-                <Circle/>
-
-
-          <ButtonAnimation />
-
+{/*
+            <NeonSocialIcon/>
+            <Circle/>
+            <ButtonAnimation/>*/}
+             <Ball />
 
         </div>
 
---
+++ src/Components/Ball/Ball.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Ball.module.css';
+export const Ball = () => {
+    return (
+        <div className={s.bar}>
+
+            <div className={s.ball}>
+
+            </div>
+            {/*<div className={s.ball_1}></div>*/}
+
+        </div>
+    );
+};
+
